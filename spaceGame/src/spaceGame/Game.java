a145c5c933d082b5efd8c8fd6d90b8efaa44e771
Mehmet Eren Tunç <104003881+mehmeterentunc@users.noreply.github.com>
2022-04-26 22:28:41 +0300

    Add files via upload

---
+++ spaceGame/src/spaceGame/Game.java
@@ -0,0 +1,28 @@
+package spaceGame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements KeyListener,ActionListener {
+
+	
+	public void actionPerformed(ActionEvent e) {				
+	}
+
+	
+	public void keyTyped(KeyEvent e) {				
+	}
+
+	
+	public void keyPressed(KeyEvent e) {				
+	}
+
+	
+	public void keyReleased(KeyEvent e) {				
+	}
+
+}
